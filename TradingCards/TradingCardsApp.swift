3e6a8323adf70cd59fa8c9a02c798775e6f3cea8
winstonwang <wwang@lcs.on.ca>
2021-11-08 11:20:43 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Winston Wang on 2021-11-08.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
